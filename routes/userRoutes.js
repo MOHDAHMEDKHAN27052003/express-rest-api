10e6b121930b030548fad0e831759ec9887affc5
mak <mohd.ahmedkhan190@gmail.com>
2026-07-20 09:26:47 +0530

    removed put method

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { putProfile, getProfile } = require('../controllers/userController');
+const { getProfile } = require('../controllers/userController');
 const { validateUser, handleValidationErrors } = require("../middleware/validation");
 const signup = require("../controllers/authController");
 
 router.post('/signup', validateUser, handleValidationErrors, signup);
 
 router.get('/:id', getProfile);
-
-router.put('/:id', validateUser, handleValidationErrors, putProfile);
 
 module.exports = router;
