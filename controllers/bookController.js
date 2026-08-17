c1504463dbda75565f5e6fe6743a9210034c3562
mak <mohd.ahmedkhan190@gmail.com>
2026-08-17 11:58:13 +0530

    pagination

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -3,13 +3,13 @@ const errorResponse = require('../utils/serverError');
 
 const createBook = async (req, res) => {
     try {
-        const { 
-            title, 
-            author, 
-            ISBN, 
+        const {
+            title,
+            author,
+            ISBN,
             publicationYear,
-            genre, 
-            quantity 
+            genre,
+            quantity
         } = req.body;
 
         const existingBook = await Book.findOne({ ISBN });
@@ -45,12 +45,39 @@ const createBook = async (req, res) => {
 
 const getAllBooks = async (req, res) => {
     try {
-        const books = await Book.find();
-        
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 4;
+
+        const { genre, author, publicationYear } = req.query;
+        const filter = {};
+
+        if (genre) filter.genre = genre;
+        if (author) filter.author = author;
+        if (publicationYear) filter.publicationYear = publicationYear;
+
+        const options = {
+            page: page,
+            limit: limit,
+            sort: { createdAt: -1 }
+        };
+
+        const result = await Book.paginate(filter, options);
+
         res.status(200).json({
             success: true,
-            data: books
+            data: result.docs,
+            pagination: {
+                currentPage: result.page,
+                totalPages: result.totalPages,
+                totalItems: result.totalDocs,
+                itemsPerPage: result.limit,
+                hasNextPage: result.hasNextPage,
+                hasPrevPage: result.hasPrevPage,
+                nextPage: result.nextPage,
+                prevPage: result.prevPage
+            }
         });
+
     } catch (error) {
         errorResponse(res, error);
     }
--- models/Book.js
+++ models/Book.js
@@ -1,31 +1,34 @@
 const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate-v2');
 
 const bookSchema = new mongoose.Schema({
-    title: { 
+    title: {
         type: String
     },
-    author: { 
-        type: String 
+    author: {
+        type: String
     },
-    ISBN: { 
-        type: Number, 
+    ISBN: {
+        type: Number,
         unique: true
     },
-    publicationYear: { 
-        type: Date 
+    publicationYear: {
+        type: Number
     },
-    genre: { 
+    genre: {
         type: String,
         enum: ['Fiction', 'Non-Fiction', 'Science Fiction', 'Fantasy', 'Mystery', 'Thriller', 'Romance', 'Horror', 'Biography', 'History', 'Science', 'Poetry', 'Drama', 'Adventure', 'Young Adult', 'Children']
     },
-    quantity: { 
+    quantity: {
         type: Number
     },
-    createdBy: { 
-        type: mongoose.Schema.Types.ObjectId, 
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
     }
 }, { timestamps: true });
+
+bookSchema.plugin(mongoosePaginate);
 
 const Book = mongoose.model('Book', bookSchema);
 module.exports = Book;
